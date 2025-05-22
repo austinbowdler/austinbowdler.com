43e6ea2921cb8c1bf6dc96933a30b22629f7149d
Austin Bowdler <abowdler2@gmail.com>
2025-05-21 19:31:18 -0500

    nuxt

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/image',
+    '@nuxt/ui'
+  ]
+})
